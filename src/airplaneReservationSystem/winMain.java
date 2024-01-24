23cb41cca62b338a2ddbfe1efb21fe81fffcf753
BlueyG <59690656+Bluarianknight@users.noreply.github.com>
2024-01-24 17:50:09 -0500

    Initial Creation of the Project
    Creating both the GitHub, as well as adding a winMain class.

---
+++ src/airplaneReservationSystem/winMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package airplaneReservationSystem;
+
+/**
+ * 
+ */
+public class winMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
