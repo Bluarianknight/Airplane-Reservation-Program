7ebfa625aeb40667d8b14df2b12e27e4812b571d
Bluarianknight <59690656+Bluarianknight@users.noreply.github.com>
2024-02-21 14:29:01 -0500

    Database organization, creation of the Authorization, and continued work.
    Description to be filled later.

--- src/Main/databaseConnection.java
+++ src/Main/databaseConnection.java
@@ -1,6 +1,8 @@
 package Main;
 import java.sql.*;
 
+import javax.swing.JOptionPane;
+
 public class databaseConnection {
 	String databaseLocation;
 	Connection connection = null;
@@ -15,6 +17,16 @@ public class databaseConnection {
 		this.databaseLocation = location;
 	}
 	
+	public void setDatabase(String data) {
+		this.databaseLocation = data;
+	}
+	public void connectDatabase() {
+		try {
+			connection = DriverManager.getConnection("jdbc:ucanaccess://" + this.databaseLocation);
+		} catch(SQLException sqlex) {
+			JOptionPane.showMessageDialog(null, sqlex.getMessage());
+		}
+	}
 	
 
 }
--- src/Main/testWindow.java
+++ src/Main/testWindow.java
@@ -1,24 +1,34 @@
 package Main;
 import javax.swing.*;
+import javax.swing.JFileChooser;
 public class testWindow extends baseWindow {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	databaseConnection database = new databaseConnection();
+	String loc;
 	public testWindow(String newTitle, int nx, int ny) {
 		super(newTitle, nx, ny);
+		setDataLocation();
 		// TODO Auto-generated constructor stub
 	}
 
 	public testWindow(String newTitle) {
 		super(newTitle);
+		setDataLocation();
 	}
 	
 	public void setDataLocation() {
 		JFileChooser filelocation = new JFileChooser();
 		filelocation.showOpenDialog(null);
+		database.setDatabase(filelocation.getName());
+		database.connectDatabase();
 		
-		try {
-			
-		}
+		
+		
+
 		
 	}
 	
